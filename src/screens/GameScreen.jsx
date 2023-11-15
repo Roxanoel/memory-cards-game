199b8b84881f8c1bafa9a0d8a957f933fc7c13a8
Roxane Noël <roxane.noel92@gmail.com>
2023-11-15 15:09:21 -0800

    Cards are getting shuffled on click

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
+import shuffle from "../utils/shuffle"
 import Card from "../components/Card"
 
 function GameScreen({data, onNewGameClicked}) {
-    const handleCardClicked = (id) => console.log('clicked ID = ' + id)
-    
+    const [dataInOrder, setDataInOrder] = useState(data)
+
+    const shuffleCards = () => {
+        const copy = [...dataInOrder]
+        const shuffledData = shuffle(copy)
+        console.log(shuffledData)
+        setDataInOrder([...shuffledData])
+    }
+
+    const handleCardClicked = (id) => {
+        console.log('clicked ID = ' + id)
+        // TO DO: Check if the card was already clicked
+        // Shuffle the cards
+        shuffleCards()
+    }
+
     return( 
-    <div>{data.map((itemData) => <Card key={itemData.id} imageUrl={itemData.imageUrl} cardId={itemData.id} onCardClicked={handleCardClicked} />)}
+    <div>
+        {dataInOrder.map((itemData) => <Card key={itemData.id} imageUrl={itemData.imageUrl} cardId={itemData.id} onCardClicked={handleCardClicked} />)}
         <button onClick={onNewGameClicked}>New Game</button>
     </div>
     )
