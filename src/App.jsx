45f922d6c93836ff9b63089a8e27e642b743e4e6
Roxane Noël <roxane.noel92@gmail.com>
2023-11-14 16:24:53 -0800

    Can initialise loading

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,23 @@ import LoadingScreen from './screens/LoadingScreen'
 import GameScreen from './screens/GameScreen'
 
 function App() {
+  const [gameState, setGameState] = useState('PREGAME')
+
+  const initiateLoading = () => changeGameState('LOADING')
+  const startGame = () => changeGameState('GAME')
+  const reinitialiseGame = () => changeGameState('PREGAME')
+
+  const changeGameState = (state) => {
+    if (state === 'PREGAME' || state === 'GAME' || state === 'LOADING') {
+      setGameState(state)
+    } else throw new Error(`state is ${state}, should be either 'PREGAME', 'GAME', or 'LOADING'`)
+  }
 
   return (
     <>
-      <PregameScreen />
-      <LoadingScreen />
-      <GameScreen /> 
+      {gameState === 'PREGAME' && <PregameScreen onStartGameClicked={initiateLoading} />}
+      {gameState === 'LOADING' && <LoadingScreen />}
+      {gameState === 'GAME' && <GameScreen />} 
     </>
   )
 }
--- src/screens/PregameScreen.jsx
+++ src/screens/PregameScreen.jsx
@@ -1,5 +1,7 @@
-function PregameScreen() {
-    return <div>pregame</div>
+function PregameScreen({onStartGameClicked}) {
+    return <div>
+        <button onClick={onStartGameClicked}>Start Game</button>
+    </div>
 }
 
 export default PregameScreen
