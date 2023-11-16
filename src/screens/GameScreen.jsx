ae31f77936d98aa9469233a895935f20d56a2969
Roxane Noël <roxane.noel92@gmail.com>
2023-11-15 16:27:34 -0800

    Implemented list of clicked ids + reset when re-click

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -4,23 +4,30 @@ import Card from "../components/Card"
 
 function GameScreen({data, onNewGameClicked}) {
     const [dataInOrder, setDataInOrder] = useState(data)
+    const [clickedItemIds, setClickedItemIds] = useState([])
 
     const shuffleCards = () => {
         const copy = [...dataInOrder]
         const shuffledData = shuffle(copy)
-        console.log(shuffledData)
         setDataInOrder([...shuffledData])
     }
 
     const handleCardClicked = (id) => {
-        console.log('clicked ID = ' + id)
         // TO DO: Check if the card was already clicked
+        if (clickedItemIds.includes(id)) {
+            // reset the array 
+            setClickedItemIds([])
+        }
+        else {// add id to the list 
+            setClickedItemIds([...clickedItemIds, id])
+        }
         // Shuffle the cards
         shuffleCards()
     }
 
     return( 
     <div>
+        <div className="scoreboard">{`Score: ${clickedItemIds.length}`}</div>
         <div className="game-board">
             {dataInOrder.map((itemData) => <Card key={itemData.id} imageUrl={itemData.imageUrl} cardId={itemData.id} onCardClicked={handleCardClicked} />)}
         </div>
