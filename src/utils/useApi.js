f80fd9257d41bce0b49701e02d0a628dc6d43f5e
Roxane Noël <roxane.noel92@gmail.com>
2023-11-20 11:18:34 -0800

    Ensured random ids wouldn't be duplicate (+ cleanup)

--- src/utils/useApi.js
+++ src/utils/useApi.js
@@ -7,23 +7,16 @@ const P = new Pokedex({cacheImages: true})
 const MIN_ID = 1   // ids start at 1
 const MAX_ID = 1000   // to make sure it won't overflow, always round down
 
-function getPokedex() {
-    return P
-}
-
 function getRandomPokemons(amount) {
     // build an array of random id's, specified length, none can be repeated
     const randomIds = []
-    for (let i = 0; i < amount; i++) {
-        // generate random id between min and max
-        const randId = Math.floor(Math.random() * (MAX_ID - MIN_ID +1)) + MIN_ID
-        // check if it's already contained in the array, if so generate another one
-        // once it's unique, add to array
-        randomIds.push(randId)
+    while(randomIds.length < amount){
+        var idCandidate = Math.floor(Math.random() * (MAX_ID - MIN_ID +1)) + MIN_ID
+        if(randomIds.indexOf(idCandidate) === -1) randomIds.push(idCandidate)
     }
     // get pokemon data for all these IDs 
     // return the data
     return randomIds
 }
 
-export { getPokedex, getRandomPokemons }
+export { getRandomPokemons }
