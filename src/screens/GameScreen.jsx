f61b85d370497b044ae3129c0256301709075394
Roxane Noël <roxane.noel92@gmail.com>
2023-11-20 14:57:28 -0800

    removed console.log

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -13,7 +13,6 @@ function GameScreen({data, onNewGameClicked}) {
     }
 
     const generateCards = () => {
-        console.log(dataInOrder)
         return dataInOrder.map((itemData) => <Card key={itemData.id} name={itemData.name} imageUrl={itemData.image} cardId={itemData.id} onCardClicked={handleCardClicked} />)
     }
 
