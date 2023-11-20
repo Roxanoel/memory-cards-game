f081cb78ca1b9bf1568fab2ce8112b34914cf297
Roxane Noël <roxane.noel92@gmail.com>
2023-11-20 15:10:06 -0800

    Styled pregame screen

--- src/screens/PregameScreen.jsx
+++ src/screens/PregameScreen.jsx
@@ -1,5 +1,10 @@
 function PregameScreen({onStartGameClicked}) {
-    return <div>
+    return <div className="pregame-screen">
+        <h1>Pokemon Memory Cards</h1>
+        <div className="instructions-container">
+            Try to click all of the cards without clicking on the same one twice! Every time you click on a card 
+            you have already clicked, your score resets.
+        </div>
         <button onClick={onStartGameClicked}>Start Game</button>
     </div>
 }
